73ada4a95b4f06bc179ed8b9d1c0707fe5d61221
Dharanidaran <dharanidaran11@gmail.com>
2024-12-20 19:25:35 +0530

    Add .env to .gitignore

--- ping_site/settings.py
+++ ping_site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yy$)8+rh2%j0$xpdw10zu8e==yo5!@2-l640^mg4nms!y4k*4o'
+SECRET_KEY = config('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
